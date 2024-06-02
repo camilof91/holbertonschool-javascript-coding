b6bad08f8b6132c016e45f4f8bc170d27d33a3c8
camilof91 <camilofetecua2011@outlook.com>
2024-06-02 18:51:03 -0500

    add write

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const filePath = process.argv[2];
-const stringToWrite = process.argv[3];
+const args = process.argv.slice(2);
 
-fs.writeFile(filePath, stringToWrite, 'utf8', (err) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log('File written successfully');
-  }
+fs.writeFile(args[0], args[1], (err) => {
+  if (err) throw err;
 });
